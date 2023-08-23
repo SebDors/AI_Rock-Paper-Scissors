72ee2f5c932edf5afd68a06c7db36e26d7ff0f25
Sébastien <dorssebastien@gmail.com>
2023-08-23 21:32:35 +0200

    Modifications to use AI model

--- NewInterface.py
+++ NewInterface.py
@@ -9,20 +9,28 @@ Color = "#FFF"
 
 # Create the layout of interface
 layout = [
-    [sg.Text(" "*72,background_color=Color), sg.Text('Opponent :', justification="right",background_color=Color,text_color="black",font=13)],
-    [sg.Text(" ",background_color=Color),
-     sg.Image(filename="Images/rock.png", key="EnemyRock",background_color=Color),
-     sg.Image(filename="Images/paper.png", key="EnemyPaper",background_color=Color),
-     sg.Image(filename="Images/scissors.png", key="EnemyScissors",background_color=Color)],
-    [sg.Text(" "*62,background_color=Color),sg.Text('          Score : 0/0', key='-DYNAMIC_TEXT-',font=16,background_color=Color,text_color="black")],
-    [sg.Text(" "*72,background_color=Color),sg.Text("Your choice : ",background_color=Color,text_color="black",font=13)],
+    [sg.Text(" "*72, background_color=Color), sg.Text('Opponent :',
+                                                      justification="right", background_color=Color, text_color="black", font=13)],
+    [sg.Text(" ", background_color=Color),
+     sg.Image(filename="Images/rock.png",
+              key="EnemyRock", background_color=Color),
+     sg.Image(filename="Images/paper.png",
+              key="EnemyPaper", background_color=Color),
+     sg.Image(filename="Images/scissors.png", key="EnemyScissors", background_color=Color)],
+    [sg.Text(" "*62, background_color=Color), sg.Text('          Score : 0/0',
+                                                      key='-DYNAMIC_TEXT-', font=16, background_color=Color, text_color="black")],
+    [sg.Text(" "*72, background_color=Color), sg.Text("Your choice : ",
+                                                      background_color=Color, text_color="black", font=13)],
     [sg.Button(image_filename="Images/rock.png", button_color=('white'), key="Rock", border_width=5),
-     sg.Button(image_filename="Images/paper.png", button_color=('white'), key="Paper", border_width=5),
+     sg.Button(image_filename="Images/paper.png",
+               button_color=('white'), key="Paper", border_width=5),
      sg.Button(image_filename="Images/scissors.png", button_color=('white'), key="Scissors", border_width=5)],
 ]
 
 # Creating window
-window = sg.Window('Pierre Feuille Ciseaux', layout, finalize=True,background_color=Color)
+window = sg.Window('Pierre Feuille Ciseaux', layout,
+                   finalize=True, background_color=Color)
+
 
 def ColorText(Result):
     """
@@ -42,11 +50,15 @@ def ColorText(Result):
     - If the result is "Draw !", the text color is set to black.
     """
     if Result == "Lost !":
-        window["-DYNAMIC_TEXT-"].update(f"{Result}   Points : {UserPoint}/{RobotPoint}", text_color='red')
+        window["-DYNAMIC_TEXT-"].update(
+            f"{Result}   Points : {UserPoint}/{RobotPoint}", text_color='red')
     elif Result == "Won !":
-        window["-DYNAMIC_TEXT-"].update(f"{Result}   Points : {UserPoint}/{RobotPoint}", text_color='green')
+        window["-DYNAMIC_TEXT-"].update(
+            f"{Result}   Points : {UserPoint}/{RobotPoint}", text_color='green')
     else:
-        window["-DYNAMIC_TEXT-"].update(f"{Result}   Points : {UserPoint}/{RobotPoint}", text_color='black')
+        window["-DYNAMIC_TEXT-"].update(
+            f"{Result}   Points : {UserPoint}/{RobotPoint}", text_color='black')
+
 
 def ColorerRobotChoice(image_path):
     """
@@ -65,6 +77,8 @@ def ColorerRobotChoice(image_path):
     return image_with_border
 
 # Highlight the choice of robot with a black outline
+
+
 def ShowRobotchoice(RobotChoice):
     """
     Update the displayed image of the robot's choice with a red outline.
@@ -80,19 +94,19 @@ def ShowRobotchoice(RobotChoice):
     window["EnemyPaper"].update(filename="Images/paper.png")
     window["EnemyScissors"].update(filename="Images/scissors.png")
     match RobotChoice:
-        case "Rock":
+        case "Rock" | 0:
             # Load "EnemyRock" image with red outline
             image_with_border = ColorerRobotChoice("Images/rock.png")
             image_bytes = io.BytesIO()
             image_with_border.save(image_bytes, format='PNG')
             window["EnemyRock"].update(data=image_bytes.getvalue())
-        case "Paper":
+        case "Paper" | 1:
             # Load "EnemyPaper" image with red outline
             image_with_border = ColorerRobotChoice("Images/paper.png")
             image_bytes = io.BytesIO()
             image_with_border.save(image_bytes, format='PNG')
             window["EnemyPaper"].update(data=image_bytes.getvalue())
-        case "Scissors":
+        case "Scissors" | 2:
             # Load "EnemyScissors" image with red outline
             image_with_border = ColorerRobotChoice("Images/scissors.png")
             image_bytes = io.BytesIO()
@@ -100,6 +114,8 @@ def ShowRobotchoice(RobotChoice):
             window["EnemyScissors"].update(data=image_bytes.getvalue())
 
 # Reset user button border color
+
+
 def ResetButtons():
     """
     Reset the color of the Rock, Paper, and Scissors buttons to white.
@@ -114,15 +130,16 @@ def ResetButtons():
     window["Paper"].update(button_color=('white'))
     window["Scissors"].update(button_color=('white'))
 
+
 while True:
     event, values = window.read()
 
     if event in (sg.WIN_CLOSED, 'Quitter'):
         break
     if event in ['Rock', 'Paper', 'Scissors']:
-        #Sending and receiving the game's result. 
+        # Sending and receiving the game's result.
         Result, UserPoint, RobotPoint, RobotChoice = Shifoumi.Game2(
-                event, UserPoint, RobotPoint)
+            event, UserPoint, RobotPoint)
         ShowRobotchoice(RobotChoice)
         ColorText(Result)
         ResetButtons()
